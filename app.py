4ecf04541345b9e47dfee34f2b8cb4e09a3adb76
Eduardo Perales <eperales04@gmail.com>
2024-01-10 22:15:12 -0700

    feat: Get All videos and CRUD playlists

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
+from datetime import date
 import googleapiclient.discovery
 import google_auth_oauthlib.flow
 from google.oauth2.credentials import Credentials
@@ -12,11 +13,18 @@ client_secrets_file = os.getenv("CLIENT_SECRET")
 
 api_service_name = "youtube"
 api_version = "v3"
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+scopes_read = ["https://www.googleapis.com/auth/youtube.readonly"]
+scopes_write = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
-def get_credentials():
+def get_credentials_read():
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
+        client_secrets_file, scopes_read)
+    credentials = flow.run_local_server(port=8000, prompt="consent")
+    return credentials
+
+def get_credentials_write():
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes_write)
     credentials = flow.run_local_server(port=8000, prompt="consent")
     return credentials
 
@@ -27,14 +35,14 @@ def get_playlists(youtube_service):
     ).execute()
     return response["items"]
 
-@app.route('/api/playlists', methods=['GET'])
+@app.route('/api/playlist', methods=['GET'])
 def api_get_playlists():
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     # Get credentials and create an API client
-    credentials = get_credentials()
+    credentials = get_credentials_read()
 
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
@@ -54,33 +62,90 @@ def api_get_playlists():
 
 @app.route('/api/playlist/<playlist_id>', methods=['GET'])
 def api_get_playlist(playlist_id):
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    # Get credentials and create an API client
-    credentials = get_credentials()
+    credentials = get_credentials_read()
+
+    youtube = googleapiclient.discovery.build(
+        "youtube", "v3", credentials=credentials)
+
+    videos_data = {"videos": []}
+    next_page_token = None
+
+    while True:
+        playlist_response = youtube.playlistItems().list(
+            part="snippet,contentDetails",
+            playlistId=playlist_id,
+            pageToken=next_page_token
+        ).execute()
+
+        videos = playlist_response["items"]
+
+        video_ids = [video["contentDetails"]["videoId"] for video in videos]
+        videos_response = youtube.videos().list(
+            part="snippet,contentDetails",
+            id=",".join(video_ids)
+        ).execute()
+
+        for video, video_data in zip(videos, videos_response["items"]):
+            video_info = {
+                "id": video["contentDetails"]["videoId"],
+                "title": video["snippet"]["title"],
+                "duration": video_data["contentDetails"]["duration"]
+            }
+            videos_data["videos"].append(video_info)
+
+        if not playlist_response.get("nextPageToken"):
+            break
+        else:
+            next_page_token = playlist_response.get("nextPageToken")
+
+    return jsonify(videos_data)
+
+@app.route('/api/playlist', methods=['POST'])
+def api_create_playlist():
+    data=request.json
+    title=data['title']
+    description=date.today().strftime("%B %d, %Y")
+
+    if not title:
+        return jsonify({"error": "Missing title"}), 400
+    
+    credentials = get_credentials_write()
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+    
+    request_body = {
+        "snippet": {
+            "title": title,
+            "description": description
+        },
+        "status": {
+            "privacyStatus": "private"
+        }
+    }
+
+    response = youtube.playlists().insert(
+        part="snippet,status",
+        body=request_body
+    ).execute()
+
+    return jsonify(response)
+
+@app.route('/api/playlist/<playlist_id>', methods=['DELETE'])
+def api_delete_playlist(playlist_id):
+    credentials = get_credentials_write()
 
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
 
-    response = youtube.playlistItems().list(
-        part="snippet,contentDetails",
-        playlistId=playlist_id
+    response = youtube.playlists().delete(
+        id=playlist_id
     ).execute()
 
-    videos = response["items"]
-    videos_data = []
+    return jsonify(response)
 
-    for video in videos:
-        video_data = {
-            "id": video["contentDetails"]["videoId"],
-            "title": video["snippet"]["title"],
-            "thumbnail": video["snippet"]["thumbnails"]["default"]["url"]
-        }
-        videos_data.append(video_data)
-
-    return jsonify(videos_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
